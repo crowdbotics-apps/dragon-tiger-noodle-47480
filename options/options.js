ebaa6985b468ac6ea7f24ed7b1bf4b91121f6200
Crowdbotics <team@crowdbotics.com>
2024-04-11 21:37:15 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dragon-tiger-noodle-47480",
+  url: "https://dragon-tiger-noodle-47480.botics.co",
+  api: "https://dragon-tiger-noodle-47480.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
